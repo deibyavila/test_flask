e0c8e291f3ff9dc7b004be7a4f91d4ebb9def4f7
Deiby Avila <deibyavila@gmail.com>
2019-12-23 11:20:40 -0500

    add contacto con form basico

--- hola.py
+++ hola.py
@@ -1,13 +1,28 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
 
+from flask import Flask, render_template, request
+#from contact_model import Contact
 app = Flask(__name__)
 
 
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    return render_template('agenda.html')
 
-@app.route('/')
-def hola():
-    return 'holaaaaaaa'
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+
+    if request.form:
+        '''contact = Contact(name=request.form.get('name'),
+                          phone=request.form.get('phone'),
+                          email=request.form.get('email'))
+
+        contact.put()'''
+
+    return render_template('add_contact.html')
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
