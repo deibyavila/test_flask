36ea8dbfc4a0b22e445fa37b826f1ad0b798cd12
Deiby Avila <deibyavila@gmail.com>
2019-12-23 11:26:19 -0500

    imprimendo en consola

--- hola.py
+++ hola.py
@@ -14,11 +14,9 @@ def contact_book():
 def add_contact():
 
     if request.form:
-        '''contact = Contact(name=request.form.get('name'),
-                          phone=request.form.get('phone'),
-                          email=request.form.get('email'))
-
-        contact.put()'''
+        print(request.form.get('name'))
+        print(request.form.get('phone'))
+        print(request.form.get('email'))
 
     return render_template('add_contact.html')
 
